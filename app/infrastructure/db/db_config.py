280f2337fb5ddc50923d8c3f92c380fc6251964f
Maite <anamaitegarciacorrea@gmail.com>
2025-07-18 19:18:45 +0200

    Conexión con PostreSQL

---
+++ app/infrastructure/db/db_config.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga las variables de entorno desde .env
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST", "db")  # por defecto 'db' desde Docker
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.infrastructure.db.db_config import Base, engine
 
 app = FastAPI(title="Hashtag Generator API")
+
+@app.on_event("startup")
+def startup():
+    print("🔌 Connecting to the database...")
+    Base.metadata.create_all(bind=engine)
+    print("✅ Database ready.")
 
 @app.get("/")
 def read_root():
