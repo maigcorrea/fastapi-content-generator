d10277653f2977d5b563715c6700b0d7731bd48f
Maite <anamaitegarciacorrea@gmail.com>
2025-07-21 17:09:14 +0200

    Configuraciónd e CORS para permitor peticiones del frontend al backend FastAPI

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.infrastructure.db.db_config import Base, engine
+from fastapi.middleware.cors import CORSMiddleware # Import CORS middleware
 from app.infrastructure.db.models.user_model import UserModel # Import the UserModel to ensure it's registered with SQLAlchemy
 from app.interfaces import user_router  # importa el router
 
@@ -12,6 +13,21 @@ def startup():
     print("✅ Database ready.")
 
 app.include_router(user_router.router)  # registra el router
+
+
+# CORS settings
+origins = [
+    "http://localhost:5173",  # Puerto por defecto de Vite (React)
+    "http://localhost:3000",  # Puerto por defecto de Next.js o CRA
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # o ["*"] para todos (solo en desarrollo)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
